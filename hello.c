ba3c7fd27c823743caabd7cb89c8d864ed23b87d
eanfran <theliteratehobbos@gmail.com>
2020-08-23 15:00:40 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,6 +3,11 @@ int main(int argc, char **argv) {
   printf("Travis Tran\n");
   //This is my name
   printf("Computer Science\n")
-  //THis is my major
+  //This is my major
+
+  printf("Ean France Coleman\n");
+  //This is Ean's name
+  printf("Computer Science & musical composition");
+  //This is Ean's major
   return 0;
 }
